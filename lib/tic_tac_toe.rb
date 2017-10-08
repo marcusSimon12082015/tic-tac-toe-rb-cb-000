69a5ea27d03f8978edc0a8cdecce531b22d3a2f0
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-08 15:20:40 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,101 @@
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(index)
+  return index.to_i - 1
+end
+
+def move(board,index,token)
+  board[index] = token
+end
+
+def position_taken?(board,index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board,index)
+  index.between?(0,8) && !position_taken?(board,index)
+end
+
+def turn(board)
+  puts "Enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  valid = nil
+  loop do
+    if valid_move?(board,index)
+      move(board,index,current_player(board))
+      display_board(board)
+      valid = true
+    else
+      puts "Enter 1-9:"
+      input = gets.strip
+      index = input_to_index(input)
+    end
+    if valid == true
+      break
+    end
+  end
+end
+
+def turn_count(board)
+  board.count{|token| token == "X" || token == "O"}
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board,combo[0])
+  end
+end
+
+def full?(board)
+  board.all? { |token| token == "X" || token == "O" }
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board)
+end
+
+def winner(board)
+  if winning_combination = won?(board)
+    board[winning_combination.first]
+  end
+end
+
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  end
+  if draw?(board)
+    puts "Cat's Game!"
+  end
+end
