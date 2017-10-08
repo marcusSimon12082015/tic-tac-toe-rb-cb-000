ec40793cd7f5eadd3a49a8613ffd8a806ec137e9
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-08 11:39:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,3 +45,10 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  board.count{|token| token == "X" || token == "O"}
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end 
