db100306e8859750faf80bbf3cf6a03a34772d42
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-08 12:45:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,10 +74,9 @@ def over?(board)
 end
 
 def winner(board)
-  winning_combo = won?(board)
-  if winning_combo
-    return board[winning_combo[0]]
-  end
+  winning_combination = won?(board)
+  player = board[winning_combination[0]]
+  return player
 end
 
 def play(board)
