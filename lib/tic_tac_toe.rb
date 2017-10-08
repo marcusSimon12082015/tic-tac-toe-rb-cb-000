a832f47cb56eabe10db6dae33fb50cb94a5d64d7
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-08 12:22:44 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -87,9 +87,8 @@ def play(board)
     if won?(board)
       puts "Congratulations #{winner(board)}!"
       break
+    elsif draw?(board) 
+      puts "Cat's Game!"      
     end
-  end
-  if draw?(board)
-    puts "Cat's Game!"
   end
 end
